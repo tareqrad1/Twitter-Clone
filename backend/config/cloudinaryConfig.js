328e332e4c8d555f9c400824793324204b340466
Tareq Radi <raditareq15@gmail.com>
2025-01-13 12:50:02 +0200

    update last route and controller with start frontend project

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -5,5 +5,5 @@ export default function cloudinaryConfig() {
         cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret:process.env.CLOUDINARY_API_SECRET
-    });
+    })
 };
